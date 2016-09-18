db14c32855ecdfba45357d36ad28b27dcbb72e53
Tony Wei <t7wei@uwaterloo.ca>
2016-09-17 20:18:47 -0700

    mysql working

--- lumohacks/src/java/testservlet.java
+++ lumohacks/src/java/testservlet.java
@@ -6,6 +6,14 @@
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -36,8 +44,32 @@ public class testservlet extends HttpServlet {
     }
     
     protected void processFetchAllFacilities(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        
+            throws ServletException, IOException, SQLException {
+
+        try{
+            
+        Class.forName("com.mysql.jdbc.Driver").newInstance();
+        String username = "adminP1f8xDA";
+        String pw = "m9uw6PHVRkvU";
+        String conn = "jdbc:mysql://127.12.25.130:3306/lumohacks?zeroDateTimeBehavior=convertToNull";
+        Connection con = DriverManager.getConnection(conn, username, pw);
+        con.setAutoCommit(false);
+
+        PreparedStatement ps = con.prepareStatement("select * from patients");
+        ResultSet rs = ps.executeQuery();
+        String result = "";
+        while(rs.next()){
+            result += rs.getString("id") + "," + rs.getString("disease") + "," + rs.getString("name") + "," + rs.getInt("age") + "," + rs.getString("city") + ";";
+        }
+        if (result.length() > 0) {
+            result = result.substring(0, result.length()-1);
+        }
+        response.getWriter().print(result);
+        con.close();
+        }
+        catch(Exception e){
+            response.getWriter().print(e);
+        }
     }
     
     /**
@@ -50,7 +82,7 @@ public class testservlet extends HttpServlet {
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+            throws ServletException, IOException, SQLException {
         
         String action = request.getParameter("action");
         switch(action){
@@ -76,7 +108,11 @@ public class testservlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        try {
+            processRequest(request, response);
+        } catch (SQLException ex) {
+            Logger.getLogger(testservlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     /**
@@ -90,7 +126,11 @@ public class testservlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        try {
+            processRequest(request, response);
+        } catch (SQLException ex) {
+            Logger.getLogger(testservlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     /**
